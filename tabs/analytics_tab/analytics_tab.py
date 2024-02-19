ea6f0e4580d1f1aac1dc3415c31b81892cc97eac
ramosd <darioramosjunior@gmail.com>
2024-02-19 14:22:18 +0800

    Implement Bar Chart in Analytics tab.

--- tabs/analytics_tab/analytics_tab.py
+++ tabs/analytics_tab/analytics_tab.py
@@ -1,8 +1,11 @@
 from PyQt6.QtWidgets import QVBoxLayout, QWidget, QLabel, QHBoxLayout, QGraphicsSimpleTextItem
 from database import Database
-from PyQt6.QtCharts import QChart, QPieSeries, QChartView, QLineSeries, QValueAxis
+from PyQt6.QtCharts import QChart, QPieSeries, QChartView, QLineSeries, QValueAxis, QBarSeries, QBarSet, \
+    QBarCategoryAxis
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QBrush, QColor, QPainter, QFont, QPen
+from PyQt6.QtGui import QBrush, QColor, QPainter, QFont, QPen, QPixmap, QLinearGradient
+from collections import defaultdict
+from threading import Thread
 
 
 class AnalyticsTab(QWidget):
@@ -28,49 +31,45 @@ class AnalyticsTab(QWidget):
         right_layout = QVBoxLayout()
         self.setLayout(outer_layout)
 
-        label1 = QLabel("This is Label 1")
-        label2 = QLabel("This is Label 2")
-
-        label3 = QLabel("""
-        
-        
-        
-        This is label 3
-        This adad
-        asdadad
-        asdada
-        
-        
-        
-        """)
-        label4 = QLabel("""
-        
-        
-        
-        
-        This is Label 4
-        
-        
-        
-        
-        
-        """)
+        self.third_quadrant = QWidget()
+        self.third_quadrant_layout = QVBoxLayout()
+        self.third_quadrant.setLayout(self.third_quadrant_layout)
 
         self.get_statistics()
+        self.display_statistics()
 
         pie_chart = PieChart(self.win_rate)
         self.equity_curve = EquityCurve(self.trade_results, self.start_port)
 
+        # Add the bar chart manually created in matplotlib
+        # bar_chart = QLabel()
+        # image_path = "my_bar_chart.png"
+        # image_profile = QPixmap(image_path).scaledToHeight(300)
+        # bar_chart.setPixmap(image_profile)
+
+        bar_chart_qt = BarChart()
+
         left_layout.addWidget(pie_chart)
-        left_layout.addWidget(label3)
+        left_layout.addWidget(self.third_quadrant)
 
         right_layout.addWidget(self.equity_curve)
-        right_layout.addWidget(label4)
+        right_layout.addWidget(bar_chart_qt)
 
         outer_layout.addLayout(left_layout, 1)
         outer_layout.addLayout(right_layout, 2)
 
         self.show()
+
+    def display_statistics(self):
+        statistics_title = QLabel("Statistics")
+        win_rate_label = QLabel("WIN_RATE")
+        ave_gain_label = QLabel("AVE_GAIN")
+        ave_loss_label = QLabel("AVE_LOSS")
+
+        self.third_quadrant_layout.addWidget(statistics_title)
+        self.third_quadrant_layout.addWidget(win_rate_label)
+        self.third_quadrant_layout.addWidget(ave_gain_label)
+        self.third_quadrant_layout.addWidget(ave_loss_label)
 
     def get_statistics(self):
         database = Database()
@@ -157,37 +156,6 @@ class EquityCurve(QWidget):
         self.trade_results = trade_results
         self.start_port = start_port
 
-        # self.line_chart = QChart()
-        # background_brush = QBrush(QColor(70, 70, 70))
-        # self.line_chart.setBackgroundBrush(background_brush)
-        #
-        # self.line_chart_series = QLineSeries()
-        # new_port = self.start_port
-        # for index, each in enumerate(self.trade_results):
-        #     self.line_chart_series.append(index, new_port)
-        #     new_port = new_port + each
-        #
-        # self.line_chart.addSeries(self.line_chart_series)
-        # self.line_chart.setTitle("Equity Curve")
-        # self.line_chart.setTitleFont(QFont("Helvetica", 14))
-        # self.line_chart.setTitleBrush(QBrush(QColor("gold")))
-        # self.line_chart.legend().hide()
-        # self.line_chart.setAnimationOptions(QChart.AnimationOption.GridAxisAnimations)
-        #
-        # # Set series color and pen properties for better visibility
-        # pen = self.line_chart_series.pen()
-        # pen.setColor(QColor("gold"))
-        # self.line_chart_series.setPen(pen)
-        #
-        # # Create a y-axis and add it to the chart
-        # y_axis = QValueAxis()
-        # self.line_chart.addAxis(y_axis, Qt.AlignmentFlag.AlignLeft)
-        # self.line_chart_series.attachAxis(y_axis)
-        # y_axis.setLabelsColor(QColor("gold"))
-        # y_axis.setGridLinePen(QPen(QColor("white"), 0.5, Qt.PenStyle.DashLine))
-        #
-        # chart_view = QChartView(self.line_chart)
-        # chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
         self.chart_view = QChartView()
 
         self.chart_view = self.load_equity_curve(self.start_port)
@@ -198,7 +166,6 @@ class EquityCurve(QWidget):
         self.layout.addWidget(self.chart_view)
 
     def load_equity_curve(self, start_port):
-        print("OH YEAH", start_port)
         line_chart = QChart()
         line_chart_series = QLineSeries()
         background_brush = QBrush(QColor(70, 70, 70))
@@ -234,3 +201,82 @@ class EquityCurve(QWidget):
         return chart_view
 
 
+class BarChart(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        # Create chart view
+        chart_view = QChartView(self)
+        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        chart = QChart()
+        chart.setAnimationOptions(QChart.AnimationOption.SeriesAnimations)
+        chart.setTitle("Per Day Return")
+        chart.setTitleBrush(QColor('white'))
+        chart.setTitleFont(QFont("Helvetica", 12))
+        background_brush = QBrush(QColor(70, 70, 70))
+        chart.setBackgroundBrush(background_brush)
+
+        series = QBarSeries()
+
+        date_and_results = self.get_per_day_trade_results()
+        per_day_results = []
+        [per_day_results.append(float(each[1])) for each in date_and_results]
+
+        for date, value in date_and_results:
+            set = QBarSet("")
+            set.append([value])
+            if value < 0:
+                set.setColor(QColor(220, 0, 0))         # color red
+                set.setBorderColor(QColor(70, 70, 70))
+            else:
+                set.setColor(QColor(0, 220, 0))         # color green
+                set.setBorderColor(QColor(70, 70, 70))
+            series.append(set)
+
+        chart.addSeries(series)
+
+        # Creating an x-axis
+        # axis_x = QValueAxis()
+        # chart.addAxis(axis_x, Qt.AlignmentFlag.AlignBottom)
+        # series.attachAxis(axis_x)
+
+        axis_y = QValueAxis()
+        axis_y_min_value = min(per_day_results) - (min(per_day_results) * 0.05)
+        axis_y_max_value = max(per_day_results) + (max(per_day_results) * 0.05)
+        axis_y.setRange(axis_y_min_value, axis_y_max_value)
+        axis_y.setLabelsColor(QColor('white'))
+        axis_y.setGridLinePen(QPen(QColor("white"), 0.5, Qt.PenStyle.DashLine))
+        chart.addAxis(axis_y, Qt.AlignmentFlag.AlignLeft)
+        series.attachAxis(axis_y)
+
+        chart_view.setChart(chart)
+        layout.addWidget(chart_view)
+
+    def get_per_day_trade_results(self):
+        database = Database()
+        database.cursor.execute("SELECT * FROM trades_results")
+        trade_results = database.cursor.fetchall()
+
+        per_day_trade_results = []
+
+        if trade_results:
+            for trade_result in trade_results:
+                per_day_trade_results.append((trade_result[1], trade_result[2]))
+
+            # Create a default dict of float values
+            sum_dict = defaultdict(float)
+
+            # Sum the values for each date
+            for date, value in per_day_trade_results:
+                sum_dict[date] += value
+
+            # Convert the default dict to a list of tuples
+            sum_list = list(sum_dict.items())
+            print(sum_list)
+
+            return sum_list
+        else:
+            return None
